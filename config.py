ddbef5f0c947b4f22775995e1e10df193b771e76
Dmitry Vaskin <vaskind@yandex.ru>
2022-11-13 12:30:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ TEST_DATAPATH = "../pochta/test_dataset_test.csv"
 cat_features = ['oper_type', 'oper_attr', 'oper_type + oper_attr', 'type',
  'class', 'mailctg',  'name_mfi', 'index_oper', 'postmark']
 fit_model = True
-MODEL_PATH = 'automl21211_2'
+MODEL_PATH = 'models'
 ss_path = 'sample_solution.csv'
 drop_cols = ["mailrank", "mailtype", "is_privatecategory"]
