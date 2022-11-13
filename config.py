7621c943c203849930454bd20bb914c6ec5bf640
Dmitry Vaskin <vaskind@yandex.ru>
2022-11-13 20:11:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ TRAIN_DATAPATH = "../pochta/train_dataset_train.csv"
 TEST_DATAPATH = "../pochta/test_dataset_test.csv"
 cat_features = ['oper_type', 'oper_attr', 'oper_type + oper_attr', 'type',
  'class', 'mailctg',  'name_mfi', 'index_oper', 'postmark']
-fit_model = True
+fit_model = False
 MODEL_PATH = 'models'
 ss_path = 'sample_solution.csv'
 drop_cols = ["mailrank", "mailtype", "is_privatecategory"]
+tresh = 0.1
